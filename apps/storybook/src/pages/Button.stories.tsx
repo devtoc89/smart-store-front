0458a377a187f7a8d561965401ceb7cf211e2e6d
devtoc89 <devtoc89@gmail.com>
2025-03-07 17:39:21 +0900

    chore: test ruleset for pr

--- apps/storybook/src/pages/Button.stories.tsx
+++ apps/storybook/src/pages/Button.stories.tsx
@@ -12,14 +12,6 @@ const meta: Meta = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  // argTypes: {
-  //   variant: { control: "select", options: Object.keys(ButtonVariants.variant) },
-  //   shape: { control: "select", options: Object.keys(ButtonVariants.shape) },
-  //   size: { control: "select", options: Object.keys(ButtonVariants.size) },
-  //   weight: { control: "select", options: Object.keys(ButtonVariants.weight) },
-  //   children: { control: "text" },
-  //   onClick: { action: "clicked" },
-  // },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: {
     // onClick: fn()
