29730fad3fe37f40005bcf995c69bf166f41a1b9
devtoc89 <devtoc89@gmail.com>
2025-03-07 17:36:48 +0900

    chore: test ruleset for pr

--- apps/storybook/src/samples/ssg-card.stories.tsx
+++ apps/storybook/src/samples/ssg-card.stories.tsx
@@ -34,7 +34,7 @@ const products = [
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta = {
-  title: "X.samples/ssg-card",
+  title: "X.samples/ssg-card-1",
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
